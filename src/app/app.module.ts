a6e12ee84aec5d59929bc38b0856c1213f6ff033
CristianB13 <cristian36200119@gmail.com>
2022-12-24 01:53:29 +0200

    add slider and about components, update app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SliderComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
