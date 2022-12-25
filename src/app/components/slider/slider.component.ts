db404d2365010ea95ea530c645e5b5da72176244
CristianB13 <cristian36200119@gmail.com>
2022-12-25 21:34:47 +0200

    add transition to slider

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -7,12 +7,19 @@ import { Component } from '@angular/core';
 })
 export class SliderComponent {
   slide: number = 0;
+  nrOfSlides: number = 3;
 
   nextSlide() {
     this.slide++;
+    this.slide = this.mod(this.slide, this.nrOfSlides)
   }
 
   previousSlide() {
     this.slide--;
+    this.slide = this.mod(this.slide, this.nrOfSlides)
+  }
+
+  mod(a:number, b:number) {
+    return ((a % b) + b) % b
   }
 }
